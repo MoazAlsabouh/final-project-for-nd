1534b818fc4985b1c49761f689cfa04a19ad1308
Moaz Alsabouh <ibrahaem05@gmail.com>
2023-08-28 22:59:56 +0300

    Add file

--- my_app/auth.py
+++ my_app/auth.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import os
 
 AUTH0_DOMAIN =  os.environ.get("AUTH0_DOMAIN")
-ALGORITHMS =  os.environ.get("ALGORITHMS")
+ALGORITHMS =  ['RS256']
 API_AUDIENCE =  os.environ.get("API_AUDIENCE")
 
 
